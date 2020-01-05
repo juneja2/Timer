f2462f35de7020fbec095b7b02881ee8c5d8a044
Parshant Juneja <junejaparshant212@gmail.com>
2020-01-05 11:58:38 -0800

    Got the hours minutes and seconds drop down menu working

--- customDropList.js
+++ customDropList.js
@@ -1,11 +1,25 @@
-var dropDownList = document.createElement("select");
-
-for(var i = 0; i < 24; i++){
-	var option = document.createElement("option");
-	option.value = i.toString();
-	option.textContent = i.toString();
-	dropDownList.appendChild(option);
+function appendDropDownValues(arr, start, end){
+	for(var i = start; i < end; i++){
+		var i_str = i.toString();
+		for(var elem of arr){
+			var child = document.createElement("option");
+			child.value = child.textContent = i_str;
+			elem.appendChild(child);
+		}	
+	}
+	console.log("Hi");
 }
+var hours = document.createElement("select");
+var minutes = document.createElement("select");
+var seconds = document.createElement("select");
 
-console.log("Hello");
-document.getElementById("timerSetting").appendChild(dropDownList);
+var arr = [hours, minutes, seconds];
+appendDropDownValues(arr, 0, 24);
+
+appendDropDownValues(arr.slice(1, 3), 24, 60);
+
+timerSetting = document.getElementById("timerSetting");
+
+for (var elem of arr){
+	timerSetting.appendChild(elem);
+}
