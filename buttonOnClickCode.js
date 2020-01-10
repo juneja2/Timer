67ecc9b4c62059f7bea9839823a270499cc6d676
Parshant Juneja <junejaparshant212@gmail.com>
2020-01-10 14:02:00 -0800

    Implemented pause, restart functionality

--- buttonOnClickCode.js
+++ buttonOnClickCode.js
@@ -1,3 +1,6 @@
+var interval, timeout;
+var hours, minutes, seconds;
+
 function update(hours, minutes, seconds){
 	if(seconds == 0){
 		seconds = 59;
@@ -13,26 +16,26 @@ function update(hours, minutes, seconds){
 	return [hours, minutes, seconds];
 }
 function showClock(){
-	var hours = document.getElementById("hours");
-	var minutes = document.getElementById("minutes");
-	var seconds = document.getElementById("seconds");
+	hours = document.getElementById("hours");
+	minutes = document.getElementById("minutes");
+	seconds = document.getElementById("seconds");
 
 	hours = parseInt(hours.options[hours.selectedIndex].value, 10);
 	minutes = parseInt(minutes.options[minutes.selectedIndex].value, 10);
 	seconds = parseInt(seconds.options[seconds.selectedIndex].value, 10);
 	
-	setTimeout(() => {$("span").hide();
+	timeout = setTimeout(() => {$("span").hide();
 					  $("#startButton").text("Pause");
-					  $("#startButton").attr("onclick", "pause()");
-					  clock(hours, minutes, seconds)}
+					  clock()}
 			   ,1000);
 }
-function clock(hours, minutes, seconds){
+function clock(){
+	$("#startButton").attr("onclick", "pause()");
 	var p = document.getElementById("time");
 	p.textContent = hours + ":" + minutes + ":" + seconds;
 	if(!(hours == 0 && minutes == 0 && seconds == 0)){
 		[hours, minutes, seconds] = update(hours, minutes, seconds);
-		setTimeout(() => {clock(hours, minutes, seconds)}, 1000);
+		timeout = setTimeout(() => {clock(hours, minutes, seconds)}, 1000);
 	}
 	else{
 		$("span").show();
@@ -40,19 +43,25 @@ function clock(hours, minutes, seconds){
 		$("#startButton").attr("onclick", "restart()");
 		/**Adding audio tag when the timer is finished*/
 		var audio = document.createElement("audio");
+		audio.setAttribute("id", "beethoven");
 		audio.src = "5thSymphony.mp3";
 		audio.volume = 0.9;
 		audio.autoplay = true;
 		document.getElementById("timerSetting").appendChild(audio);
 		/**Making finished timer aka 0:0:0 blink*/
-		let intervalId = setInterval(() => {
+		interval = setInterval(() => {
 				p.style.visibility = (p.style.visibility == "hidden" ? "" : "hidden");
 			}, 500)
 	}
 }
 function pause(){
-
+	clearTimeout(timeout);
+	$("#startButton").text("Start");
+	$("#startButton").attr("onclick", "clock()");
 }
 function restart(){
-
+	clearInterval(interval);
+	$("#beethoven").remove();
+	$("p").css("visibility", "");
+	showClock();
 }
